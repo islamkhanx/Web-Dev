e212b66dae78b0e836cdb8591cdb4f4874dbe96a
islamkhanx <143206727+islamkhanx@users.noreply.github.com>
2025-04-05 12:29:32 +0500

    lab8

---
+++ Lab8/shop_back/api/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.TextField()
+    count = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    category = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
